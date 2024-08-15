99a66ddb0466dcf7223fe14b7aff4203e92a7424
kimdonghyuk <kdh367036@naver.com>
2024-08-16 04:35:17 +0900

    first_commit

--- src/components/step/FullStep.js
+++ src/components/step/FullStep.js
@@ -6,7 +6,20 @@ import { useNavigate } from 'react-router-dom';
 
 function FullStep() {
   const [activeCells, setActiveCells] = useState(Array(9).fill(false));
+  const [time, setTime] = useState(3);
+  const [start, setStart] = useState(false);
+  useEffect(() => {
+    if (time >= 1) {
+      setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    }
 
+    if (time < 1) {
+      setStart(true);
+    }
+    return () => clearTimeout(time);
+  }, [time]);
   useEffect(() => {
     const intervalId = setInterval(() => {
       // 1, 3, 4, 6, 7, 9 중 랜덤 위치 선택
@@ -34,18 +47,28 @@ function FullStep() {
         <FiPauseCircle className="text-4xl text-black md:text-3xl opacity-15" />
       </div>
 
-      <div className="absolute top-0 left-0 right-0 bottom-0 grid grid-cols-3 grid-rows-3">
-        {activeCells.map((isActive, idx) => (
-          <div
-            key={idx}
-            className={`flex justify-center items-center p-0 m-0 h-full w-full ${
-              isActive ? 'bg-red-600' : ''
-            }`}
-          >
-            {/* 아이콘을 제거하고, 셀에 노란색 배경을 적용 */}
-          </div>
-        ))}
-      </div>
+      {!start && (
+        <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+          <span className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-red-600 font-bold">
+            {time}
+          </span>
+        </div>
+      )}
+
+      {start && (
+        <div className="absolute top-0 left-0 right-0 bottom-0 grid grid-cols-3 grid-rows-3">
+          {activeCells.map((isActive, idx) => (
+            <div
+              key={idx}
+              className={`flex justify-center items-center p-0 m-0 h-full w-full ${
+                isActive ? 'bg-red-600' : ''
+              }`}
+            >
+              {/* 아이콘을 제거하고, 셀에 노란색 배경을 적용 */}
+            </div>
+          ))}
+        </div>
+      )}
 
       <div className="court-line top"></div>
       <div className="court-line bottom"></div>
